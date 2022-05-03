a3ef7d1b640c33e8c122ec4388193a095924643f
RaulonBDN <56082236+RaulonBDN@users.noreply.github.com>
2022-05-03 20:59:17 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -3,11 +3,32 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function eventListener() {
-  const submit = document.querySelector("input[type='submit']");
+  const submit = document.querySelector("[name='rate-form']");
+  console.log(submit);
 
-  submit.addEventListener("submit", )
+  submit.addEventListener("submit", function(event) {
+    console.log(event);
+    event.preventDefault();
+    console.log("hola");
+    const thankYou = document.querySelector(".thankyou");
+    const rate = document.querySelector(".rate");
+    displayRadioValue();
+
+    rate.classList.remove("mostrar");
+
+    thankYou.classList.add("mostrar");
+  })
 }
 
-function mostrarThankYou() {
-    
+function displayRadioValue() {
+  let meta = document.querySelector(".meta-info");
+
+  let radio = document.querySelectorAll("input[type='radio']");
+
+  for(i = 0; i < radio.length; i++) {
+    if(radio[i].checked) {
+      meta.innerHTML = "You selected " + radio[i].value + " out of " + radio.length;
+    }
+  }
 }
+
