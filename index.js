0528bc5a9e2b4d05b5cb1b59d11049a1862bd70b
RaulonBDN <raulonbdn@gmail.com>
2022-05-03 14:31:50 +0200

    second commit

---
+++ index.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+  eventListener();
+});
+
+function eventListener() {
+  const submit = document.querySelector("input[type='submit']");
+
+  submit.addEventListener("submit", )
+}
+
+function mostrarThankYou() {
+    
+}
